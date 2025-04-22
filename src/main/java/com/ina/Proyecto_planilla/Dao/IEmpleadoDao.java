6219c5739b8351a9687ad541ed774ade63dff727
Kendall-Cruz <kendallsoto51@gmail.com>
2025-04-22 00:13:30 -0600

    FuncionBuscarPuestoActivo

--- src/main/java/com/ina/Proyecto_planilla/Dao/IEmpleadoDao.java
+++ src/main/java/com/ina/Proyecto_planilla/Dao/IEmpleadoDao.java
@@ -1,12 +1,22 @@
 package com.ina.Proyecto_planilla.Dao;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ina.Proyecto_planilla.Entities.Empleado;
+import com.ina.Proyecto_planilla.Entities.Puesto_empleado;
 
 public interface IEmpleadoDao extends JpaRepository<Empleado, Long> {
-    
+
     Empleado findByNombre(String nombre);
+
     Empleado findById(long id);
-    
+
+    @Query("SELECT pe FROM Puesto_empleado pe "+ "WHERE pe.empleado.id_empleado = :empleadoId " + "AND :fecha BETWEEN pe.fecha_nombramiento AND pe.fecha_vence")
+    Optional<Puesto_empleado> findActivePuestoByEmpleadoAndDate(@Param("empleadoId") Long empleadoId, @Param("fecha") LocalDate fecha);
+
 }
--- src/main/java/com/ina/Proyecto_planilla/Services/EmpleadoService.java
+++ src/main/java/com/ina/Proyecto_planilla/Services/EmpleadoService.java
@@ -1,12 +1,16 @@
 package com.ina.Proyecto_planilla.Services;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ina.Proyecto_planilla.Dao.IEmpleadoDao;
 import com.ina.Proyecto_planilla.Entities.Empleado;
+import com.ina.Proyecto_planilla.Entities.Puesto_empleado;
 
 @Service
 public class EmpleadoService implements IEmpleadoService {
@@ -17,6 +21,23 @@ public class EmpleadoService implements IEmpleadoService {
     @Override
     public List<Empleado> listarEmpleados() {
         return empleadoDao.findAll();
+    }
+
+    public Double obtenerSalarioBaseMesAnterior(Long empleadoId, LocalDate fechaPlanilla) {
+        // Calcular el primer día del mes anterior
+        YearMonth mesAnterior = YearMonth.from(fechaPlanilla).minusMonths(1);
+        LocalDate inicioMesAnterior = mesAnterior.atDay(1);
+        LocalDate finMesAnterior = mesAnterior.atEndOfMonth();
+
+        // Buscar el puesto activo en el mes anterior
+        Optional<Puesto_empleado> puestoActivo = empleadoDao.findActivePuestoByEmpleadoAndDate(
+                empleadoId, inicioMesAnterior);
+
+        if (puestoActivo.isPresent()) {
+            return puestoActivo.get().getPuesto().getSalario_base();
+        } else {
+            throw new RuntimeException("No se encontró un puesto activo para el empleado en el mes anterior.");
+        }
     }
     
 }
